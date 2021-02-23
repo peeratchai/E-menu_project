21fe04cb80527de9feba05eba7c40029a39b571e
peeratchai kittisupap <peeratck@scg.com>
2021-02-23 22:57:28 +0700

    Add newspaper page

--- components/layout.js
+++ components/layout.js
@@ -7,49 +7,42 @@ import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 
 export default function Layout({ children, home }) {
     return (
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand href="#home" style={{ color: "white !important" }}>E-Menu</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav" >
-                {/* <Nav className="mr-auto" >
-                    <Nav.Link style={{ float: "left" }} eventKey='1' href="#features">NewsPaper</Nav.Link>
-                    <Nav.Link style={{ float: "left" }} eventKey='2' href="#pricing">Menu Feeding</Nav.Link>
-                    <Nav.Link style={{ float: "left" }} eventKey='3' href="#pricing">Contact</Nav.Link>
-                    <Nav.Link style={{ float: "left" }} eventKey='4' href="#pricing">Check out</Nav.Link>
-                </Nav>
-                <Nav>
-                    <NavDropdown title="Login" id="nav-dropdown">
-                        <NavDropdown.Item eventKey="5.1">Update user profile</NavDropdown.Item>
-                        <NavDropdown.Item eventKey="5.2">Order</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav> */}
-                <Nav>
-                    <Nav.Link eventKey="1" title="Item">
-                        NewsPaper
+        <div style={{ backgroundColor: '#f3f4f5' }}>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar.Brand href="#home" style={{ color: "white !important" }}>E-Menu</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav" >
+                    <Nav>
+                        <Nav.Link eventKey="1" title="Item" href="/">
+                            NewsPaper
                     </Nav.Link>
-                </Nav>
-                <Nav>
-                    <Nav.Link eventKey="2" title="Item">
-                        Menu Feeding
+                    </Nav>
+                    <Nav>
+                        <Nav.Link eventKey="2" title="Item">
+                            Menu Feeding
                     </Nav.Link>
-                </Nav>
-                <Nav className="mr-auto">
-                    <Nav.Link eventKey="3">
-                        Contact
+                    </Nav>
+                    <Nav className="mr-auto">
+                        <Nav.Link eventKey="3">
+                            Contact
                     </Nav.Link>
-                </Nav>
-                <Nav>
-                    <NavDropdown title="Login" id="nav-dropdown">
-                        <NavDropdown.Item eventKey="5.1">Update user profile</NavDropdown.Item>
-                        <NavDropdown.Item eventKey="5.2">Order</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-                <Nav>
-                    <Nav.Link eventKey="3">
-                        Check out
+                    </Nav>
+                    <Nav>
+                        <NavDropdown title="Login" id="nav-dropdown">
+                            <NavDropdown.Item eventKey="5.1">Update user profile</NavDropdown.Item>
+                            <NavDropdown.Item eventKey="5.2">Order</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link eventKey="3">
+                            Check out
                     </Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar >
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <div className={styles.container}>
+                {children}
+            </div>
+        </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,59 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
+import Container from 'react-bootstrap/Container'
+import { Row, Col, Card, Image, Button } from 'react-bootstrap'
+import useMediaQuery from "../utils/utils";
+import menu from '../utils/menu.json'
+import StarHalfIcon from '@material-ui/icons/StarHalf';
+import StarIcon from '@material-ui/icons/Star';
+import StarOutlineIcon from '@material-ui/icons/StarOutline';
 
 export default function Home({ allPostsData }) {
+  const isBreakpoint = useMediaQuery(768)
+
+  let listMenu
+  if (!isBreakpoint) {
+    //for Mobile
+    listMenu = menu && menu.map((menu, Index) => (
+      <Row className={utilStyles.list} key={menu.title + Index}>
+        <Col xs={12} md={4} style={{ textAlign: "center" }}>
+          <Image src={menu.image} rounded />
+        </Col>
+        <Col xs={12} md={8}>
+          <Row className={utilStyles.headingLg}>
+            {menu.title}
+          </Row>
+          <Row>
+            {menu.detail}
+          </Row>
+          <Row>
+            <StarIcon style={{ color: "blue" }} />
+          </Row>
+        </Col>
+      </Row>));
+  } else {
+    listMenu = menu && menu.map((menu, Index) => (
+      <Card key={menu.title + Index}>
+        <Card.Img variant="top" src={menu.image} />
+        <Card.Body>
+          <Card.Title>{menu.title}</Card.Title>
+          <Card.Text>
+            {menu.detail}
+          </Card.Text>
+        </Card.Body>
+        <Card.Body style={{ borderTop: '1px solid rgb(223,223,223)' }}>
+          <StarIcon /><StarIcon /><StarIcon /><StarHalfIcon /><StarOutlineIcon />
+        </Card.Body>
+      </Card>
+    ));
+  }
+
   return (
     <Layout home>
-      <Head>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Peeratchai Kittisupap</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-
-      {/* Add this <section> tag below the existing <section> tag */}
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
+      <Container>
+        {listMenu}
+      </Container>
     </Layout>
   )
 }
